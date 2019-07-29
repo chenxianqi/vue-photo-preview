6a6359141d15f6ce938779deb04c70059ad227b4
chenxianqi <361554012@qq.com>
2019-07-29 11:57:04 +0800

    extend rename myExtend

--- src/lib/index.js
+++ src/lib/index.js
@@ -136,7 +136,7 @@ var vuePhotoPreview ={
 					if(isNaN(options.index)) {
 						return;
 					}
-					options=this.extend(options,opts)
+					options=this.myExtend(options,opts)
 
 					if(disableAnimation) {
 						options.showAnimationDuration = 0;
@@ -268,7 +268,7 @@ var vuePhotoPreview ={
 					return items
 
 				},
-				extend(o1, o2) {
+				myExtend(o1, o2) {
 					for (var prop in o2) {
 						o1[prop] = o2[prop];
 					}
